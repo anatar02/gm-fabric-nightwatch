ebed4b65700c7f65fa42ab3435b23f3b665f3f6b
Sean McBride <spmcbride1201@gmail.com>
2017-11-17 15:34:30 -0500

    chore: Add browser support matrix

--- conf/parallel_local.conf.js
+++ conf/parallel_local.conf.js
@@ -37,6 +37,115 @@ nightwatch_config = {
       desiredCapabilities: {
         browser: "safari"
       }
+    },
+    chrome61Win: {
+      desiredCapabilities: {
+        os: "Windows",
+        os_version: "7",
+        browser: "Chrome",
+        browser_version: "61.0"
+      }
+    },
+    chrome62Win: {
+      desiredCapabilities: {
+        os: "Windows",
+        os_version: "7",
+        browser: "Chrome",
+        browser_version: "62.0"
+      }
+    },
+    chrome61Mac: {
+      desiredCapabilities: {
+        os: "OS X",
+        os_version: "Mavericks",
+        browser: "Chrome",
+        browser_version: "61.0"
+      }
+    },
+    chrome62Mac: {
+      desiredCapabilities: {
+        os: "OS X",
+        os_version: "Mavericks",
+        browser: "Chrome",
+        browser_version: "62.0"
+      }
+    },
+    firefox55Win: {
+      desiredCapabilities: {
+        os: "Windows",
+        os_version: "7",
+        browser: "Firefox",
+        browser_version: "55.0"
+      }
+    },
+    firefox56Win: {
+      desiredCapabilities: {
+        os: "Windows",
+        os_version: "7",
+        browser: "Firefox",
+        browser_version: "56.0"
+      }
+    },
+    firefox55Mac: {
+      desiredCapabilities: {
+        os: "OS X",
+        os_version: "Mavericks",
+        browser: "Firefox",
+        browser_version: "55.0"
+      }
+    },
+    firefox56Mac: {
+      desiredCapabilities: {
+        os: "OS X",
+        os_version: "Mavericks",
+        browser: "Firefox",
+        browser_version: "56.0"
+      }
+    },
+    safari10: {
+      // Safari 10 on Sierra
+      desiredCapabilities: {
+        os: "OS X",
+        os_version: "Sierra",
+        browser: "safari",
+        browser_version: "10.1"
+      }
+    },
+    safari11: {
+      // Safari 11 on High Sierra
+      desiredCapabilities: {
+        os: "OS X",
+        os_version: "High Sierra",
+        browser: "Safari",
+        browser_version: "11.0"
+      }
+    },
+    ie11: {
+      // IE 11 on Windows 7
+      desiredCapabilities: {
+        os: "Windows",
+        os_version: "7",
+        browser: "IE",
+        browser_version: "11.0"
+      }
+    },
+    edge15: {
+      // Edge 16 on Windows 10
+      desiredCapabilities: {
+        os: "Windows",
+        os_version: "10",
+        browser: "Edge",
+        browser_version: "15.0"
+      }
+    },
+    edge16: {
+      // Edge 16 on Windows 10
+      desiredCapabilities: {
+        os: "Windows",
+        os_version: "10",
+        browser: "Edge",
+        browser_version: "16.0"
+      }
     }
   }
 };
